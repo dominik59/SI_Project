70cbb9dadeb7a7233c8a70dcf0b1214297072bdf
dominik59 <dominik-pawlik1@wp.pl>
2016-06-30 22:38:10 +0200

    Wyszukiwanie wzorców

--- public_html/script.js
+++ public_html/script.js
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+category=[];
 $('document').ready(function(){
     $.ajax({
     type: "GET",
@@ -18,9 +19,32 @@ $('document').ready(function(){
         for(var i = 0; i < users.length; i++) {
             var user = users[i];
             console.log(user);
+            category.push(user);            
         }
+        console.log(find_pattern("CZESC"));
+        get_template(find_pattern("czesc"));
     }
 });
+    
+    function find_pattern(input)
+    {
+        var counter = -1;
+        for(var i=0;i<category.length;i++)
+        {
+            console.log(category[i].childNodes[1].childNodes[0].textContent);
+            if(category[i].childNodes[1].childNodes[0].textContent.toLowerCase()==input.toLowerCase())
+            {
+                counter = i;
+            }
+            
+        }
+        return counter;
+    }
+    function get_template(which_element)
+    {
+        console.log(category[which_element].childNodes[3].childNodes[0].textContent);
+        return category[which_element].childNodes[3].childNodes[0].textContent;
+    }
 });
 
 
